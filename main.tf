e3ff9adb23d432e3c5e20f44d18a62cd3c0f703a
Ismail Alammar <ismailalammar@Ismails-MBP.fritz.box>
2023-12-12 06:23:02 +0100

    create subnet-1 in default vpc

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ provider "aws" {
     access_key = "your_access_key"
     secret_key = "your_secret_key"
 }
+
+data "aws_vpc" "default_vpc" {
+    default = true
+}
+
+resource "aws_subnet" "subnet-1" {
+    vpc_id = data.aws_vpc.default_vpc.id
+    cidr_block = "172.31.48.0/20"
+    availability_zone = "eu-west-1a"
+}
