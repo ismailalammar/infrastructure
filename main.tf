024fb640244bd81439a5348714842147fbc63d45
Ismail Alammar <ismailalammar@Ismails-MBP.fritz.box>
2023-12-12 06:39:56 +0100

    refactor: call mouldes from root file

--- main.tf
+++ main.tf
@@ -1,18 +1,31 @@
 provider "aws" {
-    region = "eu-west-1"
-    access_key = "your_access_key"
-    secret_key = "your_secret_key"
+    region = "eu-west-3"
 }
 
-data "aws_vpc" "default_vpc" {
-    default = true
-}
-
-resource "aws_subnet" "subnet-1" {
-    vpc_id = data.aws_vpc.default_vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "eu-west-1a"
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name = "subnet-1-dev"
+        Name = "${var.env_prefix}-vpc"
     }
+}
+
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
+    vpc_id = aws_vpc.myapp-vpc.id
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+}
+
+module "myapp-server" {
+    source = "./modules/webserver"
+    vpc_id = aws_vpc.myapp-vpc.id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.myapp-subnet.subnet.id
+    avail_zone = var.avail_zone
 }
