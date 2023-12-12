17e02cd4f90666b62d006995a31098788bd95415
Ismail Alammar <ismailalammar@Ismails-MBP.fritz.box>
2023-12-12 06:35:33 +0100

    add module to create subnet

--- main.tf
+++ main.tf
@@ -12,4 +12,7 @@ resource "aws_subnet" "subnet-1" {
     vpc_id = data.aws_vpc.default_vpc.id
     cidr_block = "172.31.48.0/20"
     availability_zone = "eu-west-1a"
+    tags = {
+        Name = "subnet-1-dev"
+    }
 }
